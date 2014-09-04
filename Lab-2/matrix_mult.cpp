f6710a1154544773e4a6c24fa3f333ed34fd56e5
pallavigudipati <pallavigudipati@gmail.com>
2014-09-04 16:13:26 +0530

    new file:   matrix_mult.cpp

---
+++ Lab-2/matrix_mult.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stdlib.h>
+
+#define UPPER_LIMIT 10
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	int **A = (int **) malloc(n * sizeof(int *));
+	int **B = (int **) malloc(n * sizeof(int *));
+	int **C = (int **) malloc(n * sizeof(int *));
+	for (int i = 0; i < n; ++i) {
+		A[i] = (int *) malloc(n * sizeof(int));
+		B[i] = (int *) malloc(n * sizeof(int));
+		C[i] = (int *) malloc(n * sizeof(int));
+		for (int j = 0; j < n; ++j) {
+			A[i][j] = rand() % UPPER_LIMIT;
+			B[i][j] = rand() % UPPER_LIMIT;
+			C[i][j] = 0;
+		}
+	}
+	
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			int sum = 0;
+			for (int k = 0; k < n; ++k) {
+				sum += A[i][k] * B[k][j];
+			}
+			C[i][j] = sum;
+		}
+	}	
+}
