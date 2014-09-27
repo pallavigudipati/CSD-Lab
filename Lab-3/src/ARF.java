5c695a0dcd649f5916f92e77a48fdf1c3eda437c
pallavigudipati <pallavigudipati@gmail.com>
2014-09-27 17:27:23 +0530

    modified:   src/ARF.java 	modified:   src/RRF.java

--- Lab-3/src/ARF.java
+++ Lab-3/src/ARF.java
@@ -10,4 +10,49 @@ public class ARF {
     }
 
     public Register[] registers = new Register[Global.NUM_REGISTERS];
+    public RRF rrf;
+
+    public ARF(RRF rrf) {
+        this.rrf = rrf;
+    }
+
+    public double readRegister(int registerNum) {
+        Register register = registers[registerNum];
+        if (!register.busy) {
+            return register.value;
+        } else if (rrf.renameRegisters[register.tag].valid) {
+            return rrf.renameRegisters[register.tag].value;
+        } else {
+            // TODO pass the tag to the reservation station.
+            return 0;
+        }
+    }
+
+    // Returns False if the allocation was not a success.
+    public boolean allocateRegister(int registerNum) {
+        Register register = registers[registerNum];
+        int emptyRegister = rrf.findEmptyRegister();
+        if (emptyRegister < 0) {
+            return false;
+        }
+        RRF.RenameRegister renameRegister = rrf.renameRegisters[emptyRegister];
+        renameRegister.allocate();
+        register.tag = emptyRegister;
+        register.busy = true;
+        return true;
+    }
+
+    // Returns False if the RRF is not valid yet.
+    public boolean updateRegister(int registerNum, int tag) {
+        Register register = registers[registerNum];
+        RRF.RenameRegister renameRegister = rrf.renameRegisters[tag];
+        if (!renameRegister.valid) {
+            return false;
+        }
+        register.value = renameRegister.reset();
+        if (tag == register.tag) {
+            register.busy = false;
+        }
+        return true;
+    }
 }
--- Lab-3/src/RRF.java
+++ Lab-3/src/RRF.java
@@ -7,6 +7,17 @@ public class RRF {
         public boolean valid = false;
         public boolean busy = false;
         public double value;
+
+        public double reset() {
+            valid = false;
+            busy = false;
+            return value;
+        }
+
+        public void allocate() {
+           valid = false;
+           busy = true;
+        }
     }
 
     public RenameRegister[] renameRegisters = new RenameRegister[Global.NUM_RENAME_REGISTERS];
@@ -19,5 +30,11 @@ public class RRF {
             }
         }
         return -1; // RRF is full.
+    }
+
+    public void updateRegister(int tag, double value) {
+        RenameRegister register = renameRegisters[tag];
+        register.value = value;
+        register.valid = true;
     }
 }
